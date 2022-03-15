d4952d07d173c7301c4536282edd440cc6579c4d
EternalStarCHN <44643190+EternalStarCHN@users.noreply.github.com>
2022-03-15 21:08:33 +0800

    Update

--- Software/TpLockInAmp/Core/Inc/main.h
+++ Software/TpLockInAmp/Core/Inc/main.h
@@ -58,8 +58,14 @@ void Error_Handler(void);
 /* USER CODE END EFP */
 
 /* Private defines -----------------------------------------------------------*/
-#define LED_Pin GPIO_PIN_13
-#define LED_GPIO_Port GPIOC
+#define LED1_Pin GPIO_PIN_11
+#define LED1_GPIO_Port GPIOB
+#define LED2_Pin GPIO_PIN_12
+#define LED2_GPIO_Port GPIOB
+#define LED3_Pin GPIO_PIN_13
+#define LED3_GPIO_Port GPIOB
+#define LED4_Pin GPIO_PIN_14
+#define LED4_GPIO_Port GPIOB
 /* USER CODE BEGIN Private defines */
 
 /* USER CODE END Private defines */
--- Software/TpLockInAmp/Core/Src/gpio.c
+++ Software/TpLockInAmp/Core/Src/gpio.c
@@ -50,14 +50,14 @@ void MX_GPIO_Init(void)
   __HAL_RCC_GPIOB_CLK_ENABLE();
 
   /*Configure GPIO pin Output Level */
-  HAL_GPIO_WritePin(LED_GPIO_Port, LED_Pin, GPIO_PIN_RESET);
+  HAL_GPIO_WritePin(GPIOB, LED1_Pin|LED2_Pin|LED3_Pin|LED4_Pin, GPIO_PIN_RESET);
 
-  /*Configure GPIO pin : PtPin */
-  GPIO_InitStruct.Pin = LED_Pin;
+  /*Configure GPIO pins : PBPin PBPin PBPin PBPin */
+  GPIO_InitStruct.Pin = LED1_Pin|LED2_Pin|LED3_Pin|LED4_Pin;
   GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
   GPIO_InitStruct.Pull = GPIO_PULLDOWN;
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
-  HAL_GPIO_Init(LED_GPIO_Port, &GPIO_InitStruct);
+  HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
 
 }
 
